70590f2870f7e4c46f1faf95577bec142dc4c324
Elmir Cahangirov <elmir.cahangir@gmail.com>
2026-01-22 06:26:09 +0000

    Task 1 is done by using if elif else conditionals

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+number = str(number)
+last_digit_of_number = int(number[len(number)-1])
+if last_digit_of_number > 5:
+    print(f"Last digit of {number} is {last_digit_of_number} and is greater than 5")
+elif last_digit_of_number == 0:
+    print(f"Last digit of {number} is {last_digit_of_number} and is 0")
+else:
+    print(f"Last digit of {number} is {last_digit_of_number} and is less than 6 and not 0")
