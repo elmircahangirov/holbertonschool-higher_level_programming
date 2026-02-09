b54333428a620f5cb7856e9bd0998f1cb5bb158c
Elmir Cahangirov <elmir.cahangir@gmail.com>
2026-02-09 14:54:06 +0000

    task 8 is done!

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def uppercase(c):
-    if ord(c) >= 65 and ord(c) <= 90:
-        return True
-    return False
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()
