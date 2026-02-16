b91034b8645dcec1c40564fa1eb542d1a129777b
Elmir Cahangirov <elmir.cahangir@gmail.com>
2026-02-16 07:35:14 +0000

    Task 2 is done!

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(argc, 's' if argc != 1 else ''))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
