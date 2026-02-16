460a4df8279b7d66def3aa42454a6ec74a92df72
Elmir Cahangirov <elmir.cahangir@gmail.com>
2026-02-16 08:23:41 +0000

    Task 4 is done!

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     """Replace an element in a list at a specific position without modifying the original list."""
-    # Orijinal list-in surətini yaradırıq
     new_list = my_list.copy()
-    # Əgər index düzgündürsə, element-i dəyişirik
     if idx >= 0 and idx < len(my_list):
         new_list[idx] = element
     return new_list
